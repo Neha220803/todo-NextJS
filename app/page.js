bd95154d6db74becc998529855f4d3cefd87d07b
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-05-27 13:06:03 +0530

    changes inn "developed"

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="max-w-4xl mx-auto mt-4  px-3">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold">ToDo List</h1>
-        <h4 className="italic text-secondary " >~ developed by neha ~</h4 >
+        <h4 className="italic text-gray-500 " >~ developed by neha ~</h4 >
         <AddTask />
       </div>
       <TodoTable tasks={tasks} />
